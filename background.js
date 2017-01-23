07d3d1fa8d4e93ce7f8fa7f34ce8e87ae3765dc8
Long Wei <Bontrey@gmail.com>
2017-01-23 22:28:33 +0800

    websocket send

--- background.js
+++ background.js
@@ -1,9 +1,71 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-  if (details.url !== 'about:blank') {
-    onNav(details.url);
+let starting = false;
+
+chrome.runtime.onStartup.addListener(start);
+chrome.runtime.onInstalled.addListener(start);
+chrome.runtime.onSuspend.addListener(function() {
+  if (window.socket) {
+    window.socket.close();
+    delete window.socket;
   }
 });
 
-function onNav(url) {
-  console.log(url);
+function start() {
+  if (!starting) {
+    starting = true;
+
+    window.socket = new Socket("ws://127.0.0.1:28884");
+  }
+}
+
+chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
+  if (details.url !== 'about:blank' &&
+      details.url.indexOf('chrome://') != 0) {
+    window.socket.send(details.url);
+  }
+});
+
+class Socket {
+  constructor(url) {
+    this.url = url;
+    this.sock = new WebSocket(url);
+    this.isReady = false;
+    this.queued = [];
+
+    this.sock.onopen = () => {
+      this.onOpen();
+    };
+
+    this.sock.onclose = (e) => {
+      this.onClose(e);
+    };
+
+    this.sock.onmessage = (e) => {
+      this.onMessage(e);
+    };
+  }
+
+  send(url) {
+    if (!this.isReady) {
+      this.queued.push(url);
+    } else {
+      this.sock.send(url); 
+    }
+  }
+
+  onOpen() {
+    console.log("connected to " + this.url);
+    this.isReady = true;
+    for (const url of this.queued) {
+      this.sock.send(url);
+    }
+    this.queued = [];
+  }
+
+  onClose(e) {
+    console.log("connection closed (" + e.code + ")");
+  }
+
+  onMessage(e) {
+    console.log("message received: " + e.data);
+  }
 }
